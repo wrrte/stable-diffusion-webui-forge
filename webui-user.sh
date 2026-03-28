993fcb42b7021240aa4f3d67226d250ee0b5c95a
choemj <choemj@kaist.ac.kr>
2026-03-28 14:27:56 +0900

    화면 상단의 기본 옵션 수정

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,6 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate --precision full --no-half"
 
 export python_cmd="/opt/homebrew/bin/python3.10"
 
