0ac7b566d8b553951e8df0c91deb81f2f4b7f71d
choemj <choemj@kaist.ac.kr>
2026-04-17 17:14:27 +0900

    .

--- webui.py
+++ webui.py
@@ -230,7 +230,7 @@ def auto_generate_api_client(task_file, is_once=False):
         "enable_hr": True,
         "hr_upscaler": "Latent",
         "hr_scale": 2.0,
-        "denoising_strength": 0.54,
+        "denoising_strength": 0.56,
         "hr_second_pass_steps": 15,
         "hr_cfg": 5.5,
 
