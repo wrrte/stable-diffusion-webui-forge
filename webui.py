8f0d519d472159e8a1aadc1fe6632d82522adcea
choemj <choemj@kaist.ac.kr>
2026-06-06 22:28:30 +0900

    hr_scale을 1.5로 축소. 모든 문제 해결

--- webui.py
+++ webui.py
@@ -229,9 +229,9 @@ def auto_generate_api_client(task_file, is_once=False):
         
         "enable_hr": True,
         "hr_upscaler": "Latent (bicubic)",
-        "hr_scale": 2.0,
+        "hr_scale": 1.5,
         "denoising_strength": 0.51,
-        "hr_second_pass_steps": 12,
+        "hr_second_pass_steps": 15,
         "hr_cfg": 5.5,
 
         # "forge_additional_modules": ["fixFP16ErrorsSDXLLowerMemoryUse_v10"],
